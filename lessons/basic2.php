b584f0998ba75cea6baa821ddca9fa6688e443f7
todd <tjb5185@rit.edu>
2017-12-05 16:01:54 -0500

    Added some more files and changed css classes

--- lessons/basic2.php
+++ lessons/basic2.php
@@ -1,5 +1,7 @@
-<p>The most basic regular expression consists of a single literal character, such as a. It matches the first occurrence of that character in the string. If the string is Jack is a boy, it matches the a after the J. The fact that this a is in the middle of the word does not matter to the regex engine. If it matters to you, you will need to tell that to the regex engine by using word boundaries. We will get to that later.</p>
-<p>This regex can match the second a too. It only does so when you tell the regex engine to start searching through the string after the first match. In a text editor, you can do so by using its "Find Next" or "Search Forward" function. In a programming language, there is usually a separate function that you can call to continue searching through the string after the previous match.</p>
-<p>Similarly, the regex cat matches cat in About cats and dogs. This regular expression consists of a series of three literal characters. This is like saying to the regex engine: find a c, immediately followed by an a, immediately followed by a t.</p>
-<p>Note that regex engines are case sensitive by default. cat does not match Cat, unless you tell the regex engine to ignore differences in case.</p>
-    
+<h1 class="header">Lesson 2: Literal Characters</h1>
+<div class="readable">
+    <p>The most basic regular expression consists of a single literal character, such as a. It matches the first occurrence of that character in the string. If the string is Jack is a boy, it matches the a after the J. The fact that this a is in the middle of the word does not matter to the regex engine. If it matters to you, you will need to tell that to the regex engine by using word boundaries. We will get to that later.</p>
+    <p>This regex can match the second a too. It only does so when you tell the regex engine to start searching through the string after the first match. In a text editor, you can do so by using its "Find Next" or "Search Forward" function. In a programming language, there is usually a separate function that you can call to continue searching through the string after the previous match.</p>
+    <p>Similarly, the regex cat matches cat in About cats and dogs. This regular expression consists of a series of three literal characters. This is like saying to the regex engine: find a c, immediately followed by an a, immediately followed by a t.</p>
+    <p>Note that regex engines are case sensitive by default. cat does not match Cat, unless you tell the regex engine to ignore differences in case.</p>
+</div>
--- lessons/intermediate1.php
+++ lessons/intermediate1.php
@@ -1,6 +1,29 @@
-<p>To define a character set we use brackets ([])</p>
-<p>A character set helps you find anyone of several characters. But one character set only applies to one character. The order of the items in the character set doesn’t matter. You’re basically just saying these are the items that can match.</p>
-<p>Ex. /[aeiou]/  will match any vowel.
-Ex. /f[ea]t/ will match fet and fat but not feat or feet.
-</p>
-<p></p>
+<h1 class="header">Lesson 1: Character Sets</h1>
+<div class="readable">
+    <p>To define a character set we use brackets ([])</p>
+    <p>A character set helps you find anyone of several characters. But one character set only applies to one character. The order of the items in the character set doesn’t matter. You’re basically just saying these are the items that can match.</p>
+    <p>Ex. /[aeiou]/  will match any vowel.
+    Ex. /f[ea]t/ will match fet and fat but not feat or feet.
+    </p>
+    <p>It is important to note that if you leave a space in your character set, the regex engine will interpret that as a space so…</p>
+    <p>Ex. /[I ]/ will match: I went. Notice that both the I and the space are highlighted. This is because the regex engine interpreted the space as something you’re trying to match.</p>
+    <p>A character range allows you to save some space and typing. Instead of typing out /[0123456789]/ I can type /[0-9]/ to get any number 0-9.</p>
+    <p>It is important to note that the range character “-” only looks at the two things next to it.</p>
+    <p>For example:</p>
+    <p>/[10-50]/ Will not match all numbers 10-50 it will match all numbers between 0 and 5 (0-5) or any number 0 or 1.</p>
+    <p>We can also negate a character set with the ^ symbol.</p>
+    <p>Ex. /[^aeiou]/ matches any consonant.</p>
+    <p>It is important to note that the above example will also now match spaces and other non vowel symbols like periods.</p>
+    <p>For example:</p>
+    <p>/[10-50]/ Will not match all numbers 10-50 it will match all numbers between 0 and 5 (0-5) or any number 0 or 1.</p>
+    <p>We can also negate a character set with the ^ symbol.</p>
+    <p>Ex. /[^aeiou]/ matches any consonant.</p>
+    <p>For this reason there are many shorthand character sets that are available.</p>
+
+    <table>
+        <tr>
+            <th>Shorthand</th>
+            <th>Description</th>
+        </tr>
+    </table>
+</div>
