d31cf0d19164e64883d0f471a7c054f5b5786b2c
todd <tjb5185@rit.edu>
2017-12-05 14:41:44 -0500

    Made basic lessons

---
+++ lessons/basic5.php
@@ -0,0 +1,5 @@
+<p>In some card games, the Joker is a wildcard and can represent any card in the deck. With regular expressions, you are often matching pieces of text that you don't know the exact contents of, other than the fact that they share a common pattern or structure (eg. phone numbers or zip codes).</p>
+<p>Similarly, there is the concept of a wildcard, which is represented by the . (dot) metacharacter, and can match any single character (letter, digit, whitespace, everything). You may notice that this actually overrides the matching of the period character, so in order to specifically match a period, you need to escape the dot by using a slash \.accordingly.</p>
+<p>Wildcards</p>
+<p>. (dot)</p>
+<p>will match any single character, equivalent to ? (question mark) in standard wildcard expressions. Thus, "m.a" matches "mpa" and "mea" but not "ma" or "mppa".</p>
