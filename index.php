378878f404d73d6f70fadee2f04a7ca259977a74
msweet168 <mitchellsweet168@aol.com>
2017-12-06 12:50:55 -0500

    Replaced placeholder on front page with site promo.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 
     <h1 class="header" id="intro">Everything you want to know about RegEx,
-        all in one place.<br>(words about what RegEx is and why you should learn it)</h1>
+        all in one place.<br>Regular Expressions can speed up your programming and make you stand out from the crowd. Use these lessons and quizzes to become a RegEx master in no time!</h1>
 
     <div id="startButtonDiv">
         <button type="button" onclick="window.location.href='lessons.php'" class="greenButton">Get Started</button>
