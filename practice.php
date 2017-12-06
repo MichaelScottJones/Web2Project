f4a5561e0577e2f5c3b38630e3a32c0fadbb2b45
Michael Jones <michaelscottjones0@gmail.com>
2017-12-06 15:23:17 -0500

    handled generating practice menu

--- practice.php
+++ practice.php
@@ -1,40 +1,80 @@
 <?php
 	include_once("header.inc.php");
 ?>
+<script>
+	var level = 0;
+	var sectionTitle = [
+		"Basic Practice",
+		"Intermediate Practice",
+		"AdvancedPractice"
+	]
+	var practiceTitle = [
+		[
+			"Introduction to RegEx",
+			"Literal Characters",
+			"Metacharacters",
+			"Special Characters, Wildcard"
+		],
+		[
+			"Character Sets",
+			"Repetition Metacharacters",
+			"Quantified Repetition"
+		],
+		[
+			"Back Referencing",
+			"Word Boundaries",
+			"Atomic Group",
+			"Recursion",
+			"Callbacks",
+			"Commenting"
+		]
+	]
+	function post(lesson){
+
+	}
+
+	function generateLessons(l){
+		level = l;
+		content = "";
+		for(var i=0; i<practiceTitle[level].length; i++){
+			content += '<p class="description">Practice ' + (i+1) + ": " + practiceTitle[level][i] + '</p>';
+		    content += '<button type="button" onclick="post(' + i + ')" class="smallGreenButton">Go</button>';
+		}
+		document.getElementById("practiceLinks").innerHTML = content;
+		document.getElementById("header").innerHTML = sectionTitle[level];
+		document.getElementById("navAdv").className = "navButton last local";
+		document.getElementById("navInt").className = "navButton local";
+		document.getElementById("navBeg").className = "navButton local";
+		switch (level) {
+			case 0:
+				document.getElementById("navBeg").className += " current";
+				break;
+			case 1:
+				document.getElementById("navInt").className += " current";
+				break;
+			case 2:
+				document.getElementById("navAdv").className += " current";
+				break;
+			default:
+				console.log("ERROR: Illegal Level");
+		}
+	}
+
+	window.onload = function(){
+		generateLessons(0);
+	}
+</script>
 
         <div class="localNavBar">
             <ul class="navLinks">
-              <li><a href="" class="navButton last local">Advanced</a></li>
-              <li><a href="" class="navButton local">Intermediate</a></li>
-              <li><a href="" class="navButton current local">Beginner</a></li>
+              <li><a class="navButton last local" id="navAdv" onclick="generateLessons(2);">Advanced</a></li>
+              <li><a class="navButton local" id="navInt" onclick="generateLessons(1);">Intermediate</a></li>
+              <li><a class="navButton local" id="navBeg" onclick="generateLessons(0);">Beginner</a></li>
               <li style="float:left"><a href="" class="localTitle">Practice </a></li>
             </ul>
         </div>
     </nav>
 
-    <h1 class="header">Basic Practice</h1>
-    <p class="description">Practice 1: Introduction to RegEx</p>
-    <button type="button" onclick="window.location.href='practiceInfo.php'" class="smallGreenButton">Go</button>
-
-    <p class="description">Practice 2: Practice title</p>
-    <button type="button" onclick="window.location.href='practiceInfo.php'" class="smallGreenButton">Go</button>
-
-    <p class="description">Practice 3: Practice title</p>
-    <button type="button" onclick="window.location.href='practiceInfo.php'" class="smallGreenButton">Go</button>
-
-    <p class="description">Practice 4: Practice title</p>
-    <button type="button" onclick="window.location.href='practiceInfo.php'" class="smallGreenButton">Go</button>
-
-    <p class="description">Practice 5: Practice title</p>
-    <button type="button" onclick="window.location.href='practiceInfo.php'" class="smallGreenButton">Go</button>
-
-    <p class="description">Practice 6: Practice title</p>
-    <button type="button" onclick="window.location.href='practiceInfo.php'" class="smallGreenButton">Go</button>
-
-    <p class="description">Practice 7: Practice title</p>
-    <button type="button" onclick="window.location.href='practiceInfo.php'" class="smallGreenButton">Go</button>
-
-    <p class="description">Practice 8: Practice title</p>
-    <button type="button" onclick="window.location.href='practiceInfo.php'" class="smallGreenButton">Go</button>
-
+    <h1 class="header" id="header">FILLER TEXT</h1>
+	<div id="practiceLinks"></div>
 <?php 	include_once("footer.inc.php"); ?>
